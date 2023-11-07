28b4b00ef41f942dea084bab3d33b7b164d4c762
marcospory <139775321+marcospory@users.noreply.github.com>
2023-11-06 21:15:23 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,23 @@
+// Configuração do Firebase de seu aplicativo
+//ADICIONE OS LINKS DO FIREBASE AQUI
+
+
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+    
+    
+localStorage.setItem("user_Name", user_name);
+
+window.location = "chat_room.html";
+    
+}
+
+
+
